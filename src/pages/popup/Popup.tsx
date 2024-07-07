9a66759150d85b408c3aa47dc6214828f7b1d5bd
rogerogers <rogers@rogerogers.com>
2024-07-08 01:18:59 +0800

    feat: stargazers
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 export default function Popup(): JSX.Element {
+  const [data, setData] = useState<[]>([]);
+  const [yMax, setYMax] = useState<number>(50);
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+      const url = tab?.url;
+      if (typeof url === "string") {
+        const urlObj = new URL(url);
+        const host = urlObj.host;
+        if (host !== "github.com") {
+          return;
+        }
+        const useFull = urlObj.pathname.split("/").slice(1, 3);
+        const owner = useFull[0];
+        const repo = useFull[1];
+
+        const config = {
+          method: "get",
+          maxBodyLength: Infinity,
+          url: `https://api.ossinsight.io/v1/repos/${owner}/${repo}/stargazers/history/`,
+          headers: {
+            Accept: "application/json",
+          },
+        };
+
+        axios(config)
+          .then((response) => {
+            const rows = response?.data?.data?.rows;
+            if (typeof rows !== "undefined") {
+              setData(rows);
+              setYMax(parseInt(rows[rows.length - 1]?.stargazers));
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    });
+  }, []);
+
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
+    <div className="h-full pt-10 flex justify-center">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis domain={[0, yMax]} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="stargazers" stroke="#8884d8" />
+      </LineChart>
     </div>
   );
 }
