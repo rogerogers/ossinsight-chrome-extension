73d2b2cab0880b7fbb4e5119d6dab8cc6e823985
rogerogers <rogers@rogerogers.com>
2024-07-08 13:16:19 +0800

    feat: new logo
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import {
   CartesianGrid,
   Legend,
@@ -8,7 +8,7 @@ import {
   Tooltip,
   XAxis,
   YAxis,
-} from "recharts";
+} from 'recharts';
 export default function Popup(): JSX.Element {
   const [data, setData] = useState<[]>([]);
   const [yMax, setYMax] = useState<number>(50);
@@ -16,29 +16,29 @@ export default function Popup(): JSX.Element {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const tab = tabs[0];
       const url = tab?.url;
-      if (typeof url === "string") {
+      if (typeof url === 'string') {
         const urlObj = new URL(url);
         const host = urlObj.host;
-        if (host !== "github.com") {
+        if (host !== 'github.com') {
           return;
         }
-        const useFull = urlObj.pathname.split("/").slice(1, 3);
+        const useFull = urlObj.pathname.split('/').slice(1, 3);
         const owner = useFull[0];
         const repo = useFull[1];
 
         const config = {
-          method: "get",
+          method: 'get',
           maxBodyLength: Infinity,
           url: `https://api.ossinsight.io/v1/repos/${owner}/${repo}/stargazers/history/`,
           headers: {
-            Accept: "application/json",
+            Accept: 'application/json',
           },
         };
 
         axios(config)
           .then((response) => {
             const rows = response?.data?.data?.rows;
-            if (typeof rows !== "undefined") {
+            if (typeof rows !== 'undefined') {
               setData(rows);
               setYMax(parseInt(rows[rows.length - 1]?.stargazers));
             }
